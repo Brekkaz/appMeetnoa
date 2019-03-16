445d0e2eba886c8d0b5deef2c570582e794c6b90
Breyner Mola <breyner.mola.9@gmail.com>
2019-03-16 18:44:02 -0500

    deploy commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(__dirname + '/dist/meetnoa'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/meetnoa/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
